5d86d0c8ba0df51b54badf442884c156145b6db8
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-06 16:19:39 +0100

    Adds Basic Structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
+  <TheHeader></TheHeader>
+  <TheMain></TheMain>
+  <TheFooter></TheFooter>
 </template>
 
 <script>
+import TheFooter from './components/TheFooter.vue';
+import TheHeader from './components/TheHeader.vue';
+import TheMain from './components/TheMain.vue';
+
 export default {
+    components: { TheHeader, TheMain, TheFooter }
 }
 </script>
 
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer/>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>

b06bdf883e6585fc14ed389f50ac456bdadbb36e
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-06 17:52:18 +0100

    Completed TheMainSection

--- src/components/TheMainSection.vue
+++ src/components/TheMainSection.vue
@@ -3,11 +3,11 @@
         <div class="my-alert text-white fw-bold p-3">Found {{store.paginationInfo.pages}} characters</div>
         <div class="row row-cols-1 row-cols-md-5 g-4 mt-3">
             <div v-for="singleCharacter in store.charactersList" class="col">
-                <div class="card">
+                <div class="card bg-dark">
                     <img :src="singleCharacter.image" class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title">{{singleCharacter.name}}</h5>
-                        <p class="card-text">{{singleCharacter.status}}</p>
+                        <h5 class="card-title text-white">{{singleCharacter.name}}</h5>
+                        <p class="card-text text-secondary">{{singleCharacter.status}}</p>
                     </div>
                 </div>
             </div>
